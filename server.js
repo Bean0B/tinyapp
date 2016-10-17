0d1322bf261704676ea91c54ce23dfe1d3dee3a2
vagrant <aileentobrien@gmail.com>
2016-10-17 06:00:47 +0000

    working server and shortner functioning

--- server.js
+++ server.js
@@ -1,32 +1,65 @@
-// //imprort http module
-// var http = require('http');
-// //handle sending requests and returning responses
-// function handleRequests(req, res) {
-// //return string
-// res.end('hello world');
-// }
 
-// //create the server
-// var server = http.createServer(handleRequests);
-// //start server and listen on port x
-// server.listen(8080, function() {
-//   console.log('listening on port 8080')
-// });
 
-///////////express///////////
 
-const express = require('express');
-const app = express();
-const port = 8080;
-//route our app
-const router = require('./app/routes');
-app.use('/', router);
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
 
-//start the server
-app.listen(port, function() {
-  console.log('app started');
+//functions
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for( var i=0; i < 5; i++ )
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
+app.set("view engine", "ejs");
+
+//middleware
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+//routes
+//route for homepage
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_view", templateVars);
 });
 
 
 
+app.post("/urls", (req, res) => {
+  urlDatabase[generateRandomString()] = req.body.longURL
+  console.log(req.body.longURL);
+  res.redirect("/urls");
+})
 
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
